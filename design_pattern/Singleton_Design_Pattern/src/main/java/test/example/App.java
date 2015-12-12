3e97d9ebe10a31ddbf24cbae8395d9955bd25fa4
vikas.sachdeva <vs.sachdeva365@gmail.com>
2015-12-12 15:58:00 +0530

    singleton design pattern

---
+++ design_pattern/Singleton_Design_Pattern/src/main/java/test/example/App.java
@@ -0,0 +1,12 @@
+package test.example;
+
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("Counter initial value = " + SingletonClass.getInstance().getCounter());
+		SingletonClass.getInstance().setCounter(10);
+		System.out.println("Counter new value = " + SingletonClass.getInstance().getCounter());
+		SingletonClass.getInstance().setCounter(210);
+		System.out.println("Counter final value = " + SingletonClass.getInstance().getCounter());
+	}
+}
---
+++ design_pattern/Singleton_Design_Pattern/src/main/java/test/example/SingletonClass.java
@@ -0,0 +1,35 @@
+package test.example;
+
+public class SingletonClass {
+
+	private static SingletonClass instance;
+
+	private static boolean instanceCreated;
+
+	private int counter;
+
+	private SingletonClass() {
+		counter = 0;
+
+	}
+
+	public static SingletonClass getInstance() {
+		if (!instanceCreated) {
+			synchronized (SingletonClass.class) {
+				if (!instanceCreated) {
+					instance = new SingletonClass();
+					instanceCreated = true;
+				}
+			}
+		}
+		return instance;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+}
