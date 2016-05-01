9c44d3f0900e9f0552b6dc26f60c5bd4471145fa
vikas.sachdeva <vs.sachdeva365@gmail.com>
2016-05-01 15:23:21 +0530

    Bill pugh singleton design pattern

---
+++ design_pattern/Bill_Pugh_Singleton_Design_Pattern/src/main/java/test/example/App.java
@@ -0,0 +1,14 @@
+package test.example;
+
+import test.example.singleton.SingletonClass;
+
+public class App {
+	public static void main(String[] args) {
+
+		System.out.println("Counter initial value = " + SingletonClass.getInstance().getCounter());
+		SingletonClass.getInstance().setCounter(10);
+		System.out.println("Counter new value = " + SingletonClass.getInstance().getCounter());
+		SingletonClass.getInstance().setCounter(210);
+		System.out.println("Counter final value = " + SingletonClass.getInstance().getCounter());
+	}
+}
